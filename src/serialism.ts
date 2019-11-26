5c1d2271ae9acd3e4ed7c1c0ab23af134d197c5c
Abdullah Ali <voodooattack@hotmail.com>
2019-11-26 20:44:45 +0200

    fix: Fixed an issue with typescript compilation.
    Fixed the `Constructor<T>` definition.

--- src/serialism.ts
+++ src/serialism.ts
@@ -6,6 +6,7 @@ export { serializeNative, deserializeNative };
  * Interface representing a constructor.
  */
 export interface IConstructor<T = any> {
+  prototype: T;
   new (...args: any[]): T;
 }
 
